c921f227b48349300dbb881c8f0635c97c5f918d
MarcinBudzinski26 <MarcinBudzinski26@gmail.com>
2023-12-26 17:12:01 +0000

    Added Setter/getters + constructor for Port and Game

--- src/main/java/net/pyel/models/Game.java
+++ src/main/java/net/pyel/models/Game.java
@@ -4,6 +4,17 @@ import net.pyel.utils.CustomList;
 
 public class Game {
 
+	public Game(Machine machine, String name, String publisher, String description, String developer, int releaseYear, String cover, CustomList<Port> ports) {
+		this.machine = machine;
+		this.name = name;
+		this.publisher = publisher;
+		this.description = description;
+		this.developer = developer;
+		this.releaseYear = releaseYear;
+		this.cover = cover;
+		this.ports = ports;
+	}
+
 	private Machine machine; //Original machine
 	private String name;
 	private String publisher;
@@ -13,4 +24,66 @@ public class Game {
 	private String cover;
 	private CustomList<Port> ports = new CustomList<>();
 
+	public Machine getMachine() {
+		return machine;
+	}
+	public void setMachine(Machine machine) {
+		this.machine = machine;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPublisher() {
+		return publisher;
+	}
+
+	public void setPublisher(String publisher) {
+		this.publisher = publisher;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public String getDeveloper() {
+		return developer;
+	}
+
+	public void setDeveloper(String developer) {
+		this.developer = developer;
+	}
+
+	public int getReleaseYear() {
+		return releaseYear;
+	}
+
+	public void setReleaseYear(int releaseYear) {
+		this.releaseYear = releaseYear;
+	}
+
+	public String getCover() {
+		return cover;
+	}
+
+	public void setCover(String cover) {
+		this.cover = cover;
+	}
+
+	public CustomList<Port> getPorts() {
+		return ports;
+	}
+
+	public void setPorts(CustomList<Port> ports) {
+		this.ports = ports;
+	}
 }
--- src/main/java/net/pyel/models/Port.java
+++ src/main/java/net/pyel/models/Port.java
@@ -7,9 +7,46 @@ package net.pyel.models;
  */
 public class Port {
 
-
 	private Machine machine;
 	private String developer;
 	private String releaseYear;
 	private String cover;
+
+	public Port(Machine machine, String developer, String releaseYear, String cover) {
+		this.machine = machine;
+		this.developer = developer;
+		this.releaseYear = releaseYear;
+		this.cover = cover;
+	}
+	public Machine getMachine() {
+		return machine;
+	}
+
+	public void setMachine(Machine machine) {
+		this.machine = machine;
+	}
+
+	public String getDeveloper() {
+		return developer;
+	}
+
+	public void setDeveloper(String developer) {
+		this.developer = developer;
+	}
+
+	public String getReleaseYear() {
+		return releaseYear;
+	}
+
+	public void setReleaseYear(String releaseYear) {
+		this.releaseYear = releaseYear;
+	}
+
+	public String getCover() {
+		return cover;
+	}
+
+	public void setCover(String cover) {
+		this.cover = cover;
+	}
 }
